4cefa7d54475e9e649e86659e26ef3e379f426ef
Joan <igiann@hotmail.com>
2021-11-06 21:56:12 +0000

    New GR-IPTV list Sat  6 Nov 21:56:12 GMT 2021

--- test.py
+++ test.py
@@ -15,11 +15,12 @@ def jsonfind(json_object, name):
             return dict
 
 # Create the list file
-listchannels=['France 24 English']
+listchannels=['France 24 English','BBC News']
 f = open("news.m3u", "w")
 data = '#EXTM3U\n'
 for channel in listchannels:
     info = jsonfind(json_data,channel)
-    data += '#EXTINF:-1 group-title="ΔΙΕΘΝΗ ΔΙΚΤΥΑ" tvg-name="%s" tvg-logo="%s",%s\n%s'% (info["tvg"]["id"],info["logo"],info["name"],info["url"])
+
+    data += '#EXTINF:-1 group-title="ΔΙΕΘΝΗ ΔΙΚΤΥΑ" tvg-name="%s" tvg-logo="%s",%s\n%s\n'% (info["tvg"]["id"],info["logo"],info["name"],info["url"])
 f.write(data)
 f.close() 

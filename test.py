19ef8d81d0add570df0ef930a418bfede7660bea
Joan <igiann@hotmail.com>
2021-11-07 08:57:22 +0000

    CNN

--- test.py
+++ test.py
@@ -15,7 +15,7 @@ def jsonfind(json_object, name):
             return dict
 
 # Create the list file
-listchannels=['France 24 English','BBC News','Rai 1','Rai 2','Rai 3',
+listchannels=['CNN International Europe','France 24 English','BBC News','Rai 1',
 'Rai News 24','Al Jazeera English','TV5 Monde Europe']
 f = open("news.m3u", "w")
 data = '#EXTM3U\n'

357b1bcca43fa31eaf1e74ef8ffd1339444f597b
Joan <igiann@hotmail.com>
2021-11-07 08:45:09 +0000

    Rai

--- test.py
+++ test.py
@@ -15,12 +15,13 @@ def jsonfind(json_object, name):
             return dict
 
 # Create the list file
-listchannels=['France 24 English','BBC News','Rai 1','Rai News 24','Al Jazeera English','TV5 Monde Europe']
+listchannels=['France 24 English','BBC News','Rai 1','Rai 2','Rai 3',
+'Rai News 24','Al Jazeera English','TV5 Monde Europe']
 f = open("news.m3u", "w")
 data = '#EXTM3U\n'
 for channel in listchannels:
     info = jsonfind(json_data,channel)
 
-    data += '#EXTINF:-1 group-title="ΔΙΕΘΝΗ ΔΙΚΤΥΑ" tvg-name="%s" tvg-logo="%s",%s\n%s\n'% (info["tvg"]["id"],info["logo"],info["name"],info["url"])
+    data += '#EXTINF:-1 group-title="International" tvg-name="%s" tvg-logo="%s",%s\n%s\n'% (info["tvg"]["id"],info["logo"],info["name"],info["url"])
 f.write(data)
 f.close() 
